0f66d0e7367ba0eb1d95270b7dfe340dd27471bc
siam41sarker <48723188+siam41sarker@users.noreply.github.com>
2024-05-31 00:11:18 +0500

    first commit

---
+++ questions.js
@@ -0,0 +1,332 @@
+let questionsOption = [
+    {
+        index:1,
+        question:"What does HTML stand for?",
+        answer:"hyper text markup language",
+        options:[
+            "hyper text preprocessor",
+            "hyper text markup language",
+            "hyper text multiple language",
+            "hyper text multi language",
+        ]
+    },
+    {
+        index:2,
+        question:"What does PHP stand for?",
+        answer:"hypertext preprocessor",
+        options:[
+            "hypertext preprocessor",
+            "hypertext programming",
+            "hypertext preprogramming",
+            "hometext preprocessor",
+        ]
+    },
+    {
+        index:3,
+        question:"What does CSS stand for?",
+        answer:"cascading style sheet",
+        options:[
+            "common style sheet",
+            "colorful style sheet",
+            "computer style sheet",
+            "cascading style sheet",
+        ]
+    },
+    {
+        index:4,
+        question:"What does SQL stand for?",
+        answer:"structured query language",
+        options:[
+            "Stylish question language",
+            "stylesheet query language",
+            "structured query language",
+            "statement question language",
+        ]
+    },
+    {
+        index:5,
+        question:"What does XML stand for?",
+        answer:"eXtensible markup language",
+        options:[
+            "eXtensible markup language",
+            "eXecutable markup language",
+            "eXamin multiple language",
+            "eXtra multi-program language",
+        ]
+    },
+    {
+        index:6,
+        question:"Which one is collective noun?",
+        answer:"library",
+        options:[
+            "library",
+            "captain",
+            "girl",
+            "doctor",
+        ]
+    },
+    {
+        index:7,
+        question:"Which one is compound noun?",
+        answer:"sunlight",
+        options:[
+            "bay of bengal",
+            "team",
+            "sunlight",
+            "country",
+        ]
+    },
+    {
+        index:8,
+        question:"Water has no color of its own.Here 'Water' is_",
+        answer:"Material noun",
+        options:[
+            "Common noun",
+            "Abstract noun",
+            "Proper noun",
+            "Material noun",
+        ]
+    },
+    {
+        index:9,
+        question:"Which is the noun of 'Circular'?",
+        answer:"Circle",
+        options:[
+            "Circulation",
+            "Circle",
+            "Circularly",
+            "Encircle",
+        ]
+    },
+    {
+        index:10,
+        question:"The correct noun of 'break' is_",
+        answer:"breach",
+        options:[
+            "breakful",
+            "breakdown",
+            "breach",
+            "breaking",
+        ]
+    },
+    {
+        index:11,
+        question:"Which one is a material noun?",
+        answer:"Brick",
+        options:[
+            "Brick",
+            "Book",
+            "Pirate",
+            "Pack",
+        ]
+    },
+    {
+        index:12,
+        question:"The word 'infantry' is a_",
+        answer:"collective noun",
+        options:[
+            "abstract noun",
+            "common noun",
+            "collective noun",
+            "material noun",
+        ]
+    },
+    {
+        index:13,
+        question:"Which word falls under uncountable noun?",
+        answer:"Water",
+        options:[
+            "House",
+            "Poem",
+            "Field",
+            "Water",
+        ]
+    },
+    {
+        index:14,
+        question:"Correct noun of the verb 'encourage' is_",
+        answer:"courage",
+        options:[
+            "encourage",
+            "courage",
+            "courageous",
+            "courageously",
+        ]
+    },
+    {
+        index:15,
+        question:"What is the noun of 'Horrible'?",
+        answer:"Horror",
+        options:[
+            "Horrify",
+            "Horribility",
+            "Horribly",
+            "Horror",
+        ]
+    },
+    {
+        index:16,
+        question:"What kind of noun is 'Herd'?",
+        answer:"collective",
+        options:[
+            "collective",
+            "proper",
+            "common",
+            "material",
+        ]
+    },
+    {
+        index:17,
+        question:"The word 'compassion' is a/an_",
+        answer:"Abstract noun",
+        options:[
+            "Abstract noun",
+            "Concrete noun",
+            "Proper noun",
+            "Common noun",
+        ]
+    },
+    {
+        index:18,
+        question:"Find the common noun among these_",
+        answer:"Fish",
+        options:[
+            "Class",
+            "February",
+            "Fish",
+            "Jute",
+        ]
+    },
+    {
+        index:19,
+        question:"What kind of noun is 'Armies'?",
+        answer:"common noun",
+        options:[
+            "abstract noun",
+            "common noun",
+            "material noun",
+            "collective noun",
+        ]
+    },
+    {
+        index:20,
+        question:"Which of the following is noun?",
+        answer:"guise",
+        options:[
+            "practise",
+            "advise",
+            "guise",
+            "advertise",
+        ]
+    },
+    {
+        index:21,
+        question:"Synonym of 'audacious' is_",
+        answer:"Valiant",
+        options:[
+            "Polite",
+            "Cowardly",
+            "Valiant",
+            "Amity",
+        ]
+    },
+    {
+        index:22,
+        question:"Synonym of 'avaricious' is_",
+        answer:"Rapacious",
+        options:[
+            "Rapacious",
+            "Local",
+            "Magnanimous",
+            "Antipathy",
+        ]
+    },
+    {
+        index:23,
+        question:"Synonym of 'apocryphal' is_",
+        answer:"spurious",
+        options:[
+            "genuine",
+            "bona fide",
+            "veracious",
+            "spurious",
+        ]
+    },
+    {
+        index:24,
+        question:"Antonym of 'Colossal' is_",
+        answer:"Tiny",
+        options:[
+            "Cyclopean",
+            "Cosmos",
+            "Tiny",
+            "Vast",
+        ]
+    },
+    {
+        index:25,
+        question:"The word 'Cogent' means_",
+        answer:"convincing",
+        options:[
+            "convincing",
+            "irrelevant",
+            "good",
+            "precious",
+        ]
+    },
+    {
+        index:26,
+        question:"____ is a scarcity or lack of something.",
+        answer:"Dearth",
+        options:[
+            "Solvency",
+            "Excess",
+            "Dearth",
+            "Pomp",
+        ]
+    },
+    {
+        index:27,
+        question:"The antonym of deceitful' is_",
+        answer:"honest",
+        options:[
+            "false",
+            "honest",
+            "delusive",
+            "tricky",
+        ]
+    },
+    {
+        index:28,
+        question:"Choose the right antonym of “Eclipse”_",
+        answer:"brighten",
+        options:[
+            "dim",
+            "blot",
+            "darkness",
+            "brighten",
+        ]
+    },
+    {
+        index:29,
+        question:"Synonym of 'Emancipate' is_",
+        answer:"liberate",
+        options:[
+            "bound",
+            "enslave",
+            "liberate",
+            "conduct",
+        ]
+    },
+    {
+        index:30,
+        question:"These ___ are mine. What will be the answer?",
+        answer:"poultry",
+        options:[
+            "poultry",
+            "poultries",
+            "poultres",
+            "poultrys",
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,197 @@
+const myBtn = document.querySelector(".MyBtn button");
+const rules = document.querySelector(".rules");
+const exitBtn = document.querySelector(".exitBtn");
+const cont = document.querySelector(".cont")
+const questions = document.querySelector(".Questions");
+const timePart = document.querySelector(".time_section .seconds");
+const scrollingPart = document.querySelector(".Questions .scrolling");
+const Result_box = document.querySelector(".Result_box");
+const scoreX = document.querySelector(".score");
+const restart1 = document.querySelector(".Result_button .restart1");
+const timeLeft = document.querySelector(".timeLeft");
+myBtn.onclick = ()=>
+    {
+        rules.classList.add("activeRules");
+    }
+exitBtn.onclick = ()=>
+    {
+        rules.classList.remove("activeRules");
+    }
+cont.onclick = ()=>
+    {
+        rules.classList.remove("activeRules");
+        questions.classList.add("activeQuiz");
+        showQuestions(0);
+        setTimingPart(15);
+        setScrollingPart(0);
+    }
+let FooterButton = document.querySelector(".FooterButton");
+let ques_count = 0;
+let TimeCounter;
+let scrollCounter;
+let timeValue = 15;
+let scrollValue =0;
+let scoreCount = 0;
+let rightAnswer=0;
+let wrongAnswer=0;
+let wr=0;
+FooterButton.onclick = ()=>
+        {
+            if(ques_count<questionsOption.length-1)
+                {
+                    ques_count++;
+                    showQuestions(ques_count);
+                    clearInterval(TimeCounter);
+                    setTimingPart(timeValue);
+                    clearInterval(scrollCounter);
+                    setScrollingPart(scrollValue);
+                    FooterButton.style.display = "none";
+                }
+            else
+                {
+                   
+                    console.log("You've completed all the questions.");
+                    showResultBox();
+
+                }
+        }
+function showQuestions(index)
+    {
+        let quest = document.querySelector(".xyz");
+        let myOptions = document.querySelector(".myOptions");
+        let footerLeft = document.querySelector(".footerLeft");
+        let qstSpan ='<span class="ques">'+questionsOption[index].index+". "+questionsOption[index].question+"</span>";
+        quest.innerHTML = qstSpan;
+        let optionTag = '<div class="options"><span>'+questionsOption[index].options[0]+'</span></div>'+'<div class="options"><span>'+questionsOption[index].options[1]+'</span></div>'+'<div class="options"><span>'+questionsOption[index].options[2]+'</span></div>'+'<div class="options"><span>'+questionsOption[index].options[3]+'</span></div>';
+        myOptions.innerHTML = optionTag;
+        let total_question = "<p>"+questionsOption[index].index+" out of "+questionsOption.length+" Questions.</p>";
+        footerLeft.innerHTML = total_question;
+        let optionTaken = myOptions.querySelectorAll(".options");
+        let i;
+        for(i=0;i<optionTaken.length;i++)
+            {
+                optionTaken[i].setAttribute("onclick","optionSelected(this)")
+            }
+    }
+let tickIcon = '<div class="tick icon"><i class="fa-solid fa-check"></i></div>';
+let crossIcon = '<div class="cross icon"><i class="fa-solid fa-circle-xmark"></i></div>';
+function optionSelected(ans)
+    {
+        clearInterval(TimeCounter);
+        clearInterval(scrollCounter);
+        FooterButton.style.display = "block";
+        let selected = ans.textContent;
+        let correctAns = questionsOption[ques_count].answer;
+        let option_list = document.querySelector(".myOptions");
+        let optionSorted = option_list.children.length;
+        let i;
+        if(selected==correctAns)
+            {
+                rightAnswer++;
+                console.log(scoreCount);
+                ans.classList.add("corrected");
+                console.log("Answer is correct.");
+                ans.insertAdjacentHTML("beforeend",tickIcon);
+            }
+        else
+            {
+                wr++;
+                wrongAnswer+=0.5;
+                ans.classList.add("wrongAns");
+                console.log("Wrong answer.");
+                ans.insertAdjacentHTML("beforeend",crossIcon);
+                let m;
+                for(m=0;m<optionSorted;m++)
+                    {
+                        if(option_list.children[m].textContent==correctAns)
+                            {
+                                option_list.children[m].setAttribute("class","options corrected");
+                                option_list.children[m].insertAdjacentHTML("beforeend",tickIcon);
+                            }
+                    }
+            }
+        for(i=0;i<optionSorted;i++)
+            {
+                option_list.children[i].classList.add("disabled");
+            }  
+        if(questionsOption[ques_count].index==questionsOption.length)
+                    {
+                        FooterButton.textContent = "result";
+                    }
+       
+    }
+function setTimingPart(value)
+    {
+        TimeCounter = setInterval(timerFunc,1000);
+        function timerFunc()
+            {
+                timePart.textContent = value;
+                value--;
+                if(value<9)
+                    {
+                        let perfectaTime = timePart.textContent;
+                        timePart.textContent = "0"+perfectaTime;
+                    }
+                if(value<0)
+                    {
+                        FooterButton.style.display = "block";
+                        timePart.textContent = "00";
+                        clearInterval(TimeCounter); 
+                        let option_off =document.querySelector(".myOptions");
+                        let n;
+                        for(n=0;n<option_off.children.length;n++)
+                            {
+                                option_off.children[n].classList.add("disabled");
+                            }
+                        if(questionsOption[ques_count].index==questionsOption.length)
+                            {
+                                    FooterButton.textContent = "result";
+                            }
+                        alert("Time's up! You can't select any option.");
+                        
+                    }
+            }
+    }
+function setScrollingPart(scroll_value)
+    {
+        scrollCounter = setInterval(scrollFunc,50);
+        function scrollFunc()
+            {
+                scrollingPart.style.width = scroll_value+"px";
+                scroll_value++;
+                if(scroll_value>319)
+                    {
+                        clearInterval(scrollCounter);
+                    }
+            }
+    }
+function showResultBox()
+    {
+        rules.classList.remove("activeRules");
+        questions.classList.remove("activeQuiz");
+        Result_box.classList.add("activeResult");
+        scoreCount=rightAnswer-wrongAnswer;
+        if(scoreCount>=20)
+            {
+                let scoringPart ='<p style="color:#155724;font-weight:bold;margin-left:47px;">Correct Answer: '+rightAnswer+'</p><p style="color:#721c24;font-weight:bold;margin-left:47px;">Wrong Answer:  '+wr+'</p>'+'<span style="color:#155724;font-weight:bold;">'+'Congratulations! </span><span style="margin-left:-2px">'+"You've got  "+'<p style="display:inline">'+scoreCount+'</p> out of <p style="display:inline">'+questionsOption.length+"</p></span>";                
+                scoreX.innerHTML=scoringPart;
+            }
+        else if(scoreCount>=10 && scoreCount<20)
+            {
+                let scoringPart ='<p style="color:#155724;font-weight:bold;margin-left:47px;">Correct Answer: '+rightAnswer+'</p><p style="color:#721c24;font-weight:bold;margin-left:47px;">Wrong Answer:  '+wr+'</p>'+'<span style="color:#4f2082;font-weight:bold;">'+'Good Effort! </span><span style="margin-left:-2px">'+"You've got  "+'<p style="display:inline">'+scoreCount+'</p> out of <p style="display:inline">'+questionsOption.length+"</p></span>";
+                scoreX.innerHTML=scoringPart;
+            }
+        else
+            {
+                if(scoreCount<0)
+                    {
+                        scoreCount=0;
+                    }
+                let scoringPart ='<p style="color:#155724;font-weight:bold;margin-left:47px;">Correct Answer: '+rightAnswer+'</p><p style="color:#721c24;font-weight:bold;margin-left:47px;">Wrong Answer:  '+wr+'</p>'+'<span style="color:#721c24;font-weight:bold;">'+'Sorry! </span><span style="margin-left:-2px">'+"You've got  "+'<p style="display:inline">'+scoreCount+'</p> out of <p style="display:inline">'+questionsOption.length+"</p></span>";
+                scoreX.innerHTML=scoringPart;
+            }
+    }
+restart1.onclick = ()=>
+    {
+            window.location.reload();
+    }
